e2cb49cf0896eefdd0630f1ebc7bccdec41542da
wonseok oh <wonseokoh@wonseokui-MacBookPro.local>
2022-10-22 23:58:19 +0900

    set eslint

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
+app.get("/hello", (req, res) => {
+  console.log(req.url);
+  return res.json({ message: "hello world" });
+});
 
-
-app.listen(3000, ()=> {
-    console.log('listening...')
-})
+app.listen(3000, () => {
+  console.log("listening...");
+});
